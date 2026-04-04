84b73dc05b9dbdeba35dc0beb2fee500117b136a
Gokhan Kurt <krtgkn@gmail.com>
2026-04-04 21:21:18 +0300

    add site config

--- packages/website/astro.config.ts
+++ packages/website/astro.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://gkurt.com/tegaki',
   integrations: [
     starlight({
       title: 'Tegaki',
